df2c6e80c270643dba409be4f558aaaf1847de24
dewble <dewble62@gmail.com>
2022-01-18 01:47:07 +0900

    post_list.html 작성

--- frontend2/src/pages/AppPostDetail.vue
+++ frontend2/src/pages/AppPostDetail.vue
@@ -1,59 +1,6 @@
 <template>
   <v-app id="inspire">
-    <!-- drawer -->
-    <v-navigation-drawer v-model="drawer" app clipped>
-      <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <!-- app-bar -->
-    <v-app-bar app clipped-left color="indigo" dark>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Vue.js + Django CRUD Board</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn text href="/">HOME</v-btn>
-      <v-btn text href="/post_list.html">POST LIST</v-btn>
-      <v-btn text href="/post_detail.html">POST Detail</v-btn>
-
-      <v-spacer></v-spacer>
-
-      <v-menu offset-y left bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn text v-bind="attrs" v-on="on">
-            <v-icon>mdi-account</v-icon>
-            Annoymous
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item>
-            <v-list-item-title>Login</v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-title>Register</v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-title>Logout</v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-title>Password change</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-app-bar>
+    <MainMenu></MainMenu>
 
     <v-main>
       <PostDetail></PostDetail>
@@ -67,17 +14,18 @@
 </template>
 
 <script>
+import MainMenu from "../components/MainMenu.vue"
 import PostDetail from "../components/PostDetail.vue";
 
 export default {
-  components: { PostDetail },
+  components: { MainMenu, PostDetail },
   data: () => ({
-    drawer: null,
-    items: [
-      { title: "Dashboard", icon: "mdi-view-dashboard" },
-      { title: "Photos", icon: "mdi-image" },
-      { title: "About", icon: "mdi-help-box" },
-    ],
+    // drawer: null,
+    // items: [
+    //   { title: "Dashboard", icon: "mdi-view-dashboard" },
+    //   { title: "Photos", icon: "mdi-image" },
+    //   { title: "About", icon: "mdi-help-box" },
+    // ],
   }),
 };
 </script>
